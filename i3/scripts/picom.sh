7c35639560aebc14e0829a049d688894f14cf45e
user <user@localhost>
2024-06-05 11:18:48 +0200

    Updates to i3

---
+++ i3/scripts/picom.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q picom
+
+# Wait until the processes have been shut down
+while pgrep -x picom >/dev/null; do sleep 1; done
+
+# Launch polybar
+picom &
