71f3c33af873a85e6dd7559ceea2b17303f9a580
user <user@localhost>
2024-06-05 13:44:13 +0200

    Multi-monitor polybar

--- i3/scripts/polybar.sh
+++ i3/scripts/polybar.sh
@@ -7,4 +7,10 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
