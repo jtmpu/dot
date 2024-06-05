dd12acae863e288e86d69db6fc96c8ae0fed092e
user <user@localhost>
2024-06-05 10:58:07 +0200

    Update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,37 +9,37 @@ local font = wezterm.font "JetBrains Mono"
 local leader = { key = "v", mods = "CTRL" }
 local keybinds = {
     -- Tab management
-    { key = '&', mods = "LEADER|SHIFT", action = wezterm.action.CloseCurrentTab { confirm = false } },
-    { key = 'c', mods = "LEADER", action = wezterm.action.SpawnTab "CurrentPaneDomain" },
-    { key = "w", mods = "LEADER", action = wezterm.action.ShowTabNavigator },
-    { key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
-    { key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = '&',          mods = "LEADER|SHIFT",                   action = wezterm.action.CloseCurrentTab { confirm = false } },
+    { key = 'c',          mods = "LEADER",                         action = wezterm.action.SpawnTab "CurrentPaneDomain" },
+    { key = "w",          mods = "LEADER",                         action = wezterm.action.ShowTabNavigator },
+    { key = "n",          mods = "LEADER",                         action = wezterm.action.ActivateTabRelative(1) },
+    { key = "p",          mods = "LEADER",                         action = wezterm.action.ActivateTabRelative(-1) },
 
     -- Pane management
-    { key = '"', mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
-    { key = '%', mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
-    { key = 'x', mods = "LEADER", action = wezterm.action.CloseCurrentPane { confirm = false } },
-    { key = 'z', mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
-    { key = "UpArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-    { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
-    { key = "DownArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
-    { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
-    { key = "LeftArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-    { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
-    { key = "RightArrow", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
-    { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = '"',          mods = "LEADER|SHIFT",                   action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" } },
+    { key = '%',          mods = "LEADER|SHIFT",                   action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" } },
+    { key = 'x',          mods = "LEADER",                         action = wezterm.action.CloseCurrentPane { confirm = false } },
+    { key = 'z',          mods = "LEADER",                         action = wezterm.action.TogglePaneZoomState },
+    { key = "UpArrow",    mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "k",          mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "DownArrow",  mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "j",          mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "LeftArrow",  mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "h",          mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "RightArrow", mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "l",          mods = "LEADER",                         action = wezterm.action.ActivatePaneDirection("Right") },
 
     -- Copy/pasting
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo "Clipboard" },
-    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom "Clipboard" },
-    { key = "x", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCopyMode },
-    
+    { key = "c",          mods = "CTRL|SHIFT",                     action = wezterm.action.CopyTo "Clipboard" },
+    { key = "v",          mods = "CTRL|SHIFT",                     action = wezterm.action.PasteFrom "Clipboard" },
+    { key = "x",          mods = "CTRL|SHIFT",                     action = wezterm.action.ActivateCopyMode },
+
     -- Global
-    { key = "P", mods = "LEADER|SHIFT", action = wezterm.action.ActivateCommandPalette },
-    { key = "F11", action = wezterm.action.ToggleFullScreen },
-    { key = "R", mods = "CTRL|SHIFT", action = wezterm.action.ReloadConfiguration },
-    { key = "PageUp", mods = "SHIFT", action = wezterm.action.ScrollByPage(-1) },
-    { key = "PageDown", mods = "SHIFT", action = wezterm.action.ScrollByPage(1) },
+    { key = "P",          mods = "LEADER|SHIFT",                   action = wezterm.action.ActivateCommandPalette },
+    { key = "F11",        action = wezterm.action.ToggleFullScreen },
+    { key = "R",          mods = "CTRL|SHIFT",                     action = wezterm.action.ReloadConfiguration },
+    { key = "PageUp",     mods = "SHIFT",                          action = wezterm.action.ScrollByPage(-1) },
+    { key = "PageDown",   mods = "SHIFT",                          action = wezterm.action.ScrollByPage(1) },
 }
 
 -- Define supported domains
@@ -53,11 +53,11 @@ local ssh_domains = {
 
 local default_prog = { "/bin/bash" }
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-	default_prog = { "powershell.exe", "-NoLogo" }
+    default_prog = { "powershell.exe", "-NoLogo" }
 end
 
--- disable ligatures != 
-local harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+-- disable ligatures !=
+local harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 return {
     initial_cols = 120,
