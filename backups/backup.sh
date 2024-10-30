37f31210e381947f18aec4508e4b0d4d20b1f60d
user <user@localhost>
2024-10-30 07:36:26 +0100

    Update backup

--- backups/backup.sh
+++ backups/backup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+sftp root@128.199.53.111:/home/factorio/saves/funtime.zip /home/user/gstorage/factorio/funtime.zip
 rclone sync --gcs-bucket-policy-only /home/user/gstorage encrypt: && date '+%y-%m-%d %H:%M' > $HOME/.config/backups/successful.txt
