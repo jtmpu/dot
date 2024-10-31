f1f67795f2c02b6482f687c6e3e750d25166cdc4
Joakim Valberg <joakim.valberg@embark-studios.com>
2024-10-31 10:26:19 +0100

    Remove cmp

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -3,8 +3,8 @@ local M = {}
 ---@param config vim.lsp.ClientConfig
 function M.start(config)
     local bufnr = vim.api.nvim_get_current_buf()
-    local cmp = require('cmp')
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- local cmp = require('cmp')
+    -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     local merged = vim.tbl_deep_extend(
         'force',
